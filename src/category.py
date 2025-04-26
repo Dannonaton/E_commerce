def089e9433f01d9b5e6b669511dcd415b520cfb
aleksandr <aleksander.kryzhanovsky@yandex.ru>
2025-04-26 14:31:57 +0300

    homework 15.1

--- src/category.py
+++ src/category.py
@@ -1,3 +1,4 @@
+from typing import Any
 from src.product import Product
 
 
@@ -17,17 +18,28 @@ class Category:
         Category.product_count += len(products)
         print(Category.product_count)
 
-    def add_product(self, product: dict):
+    def __str__(self):
+        total_products = 0
+        for product in self.__products:
+            total_products += product.quantity
+        return f"{self.name}, количество продуктов: {total_products} шт."
+
+    def add_product(self, product: Product) -> Any:
         """Метод добавления нового продукта в список"""
-        if isinstance(product, Product):
-            self.__products.append(product)
-            Category.product_count += 1
-        else:
-            raise TypeError
+        self.__products.append(product)
+        Category.product_count += 1
 
     @property
-    def products(self) -> str:
+    def get_product_list(self) -> str:
         product_list = ""
         for product in self.__products:
-            product_list += f"{product.name}, {product.price} руб. Остаток: {product.quantity} шт. \n "
+            product_list += f"{str(product)}\n"
+        return product_list
+
+
+    @property
+    def products(self) -> list:
+        product_list = []
+        for product in self.__products:
+            product_list.append(product)
         return product_list
